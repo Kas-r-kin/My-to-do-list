8d562b8fdb1da58bf234b82f56ac151308ae8750
Romain PHELIZON <rophelizon@etud.rcisima.isima.fr>
2015-11-23 19:15:35 +0100

    vtff

---
+++ Version 0/traitement.php
@@ -0,0 +1,60 @@
+<!-- http://fc.isima.fr/~rophelizon -->
+
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="traitement.css" />
+	<body>
+		<form method="post" action="index.php">
+			
+			<p class="Interface2">
+				<input type="submit" value="Déconnexion" />
+			</p>
+			
+  		<p><?php echo $_POST['pseudo'];
+  		echo $_POST['pass'];
+  		
+  		function checkParam($param)
+		{
+			return isset($param) && !empty($param);
+		}
+		
+		
+		function checkUser($param)
+		{
+			$validation = 0;
+			$BDD = fopen('BDD.txt', 'r+');
+			
+			while(feof($BDD))
+			{
+				$ligne = fgets($BDD);
+				echo $ligne;
+				
+				if($ligne == $param)
+				{
+					$validation = 1;
+				}
+			}
+			fclose($BDD);
+			return $validation;
+		}
+		
+		if (!checkParam($pseudo) && !checkParam($pass))
+		{
+			if (!checkUser($pseudo, $pass))
+			{
+				echo "Bonjour";
+				session_start();
+				$_SESSION['auth']=true;
+				$_SESSION['login']=$pseudo;
+			}
+			else{ echo 'Utilisateur inconnu';}
+		}
+		else{ echo 'il manque quelque chose!';}
+  		
+  		?></p>
+
+  		<!--<p> <a href="index.php"> <input type="submit" value="Déconnexion" /></a> </p>-->
+  		
+  		</form>
+	</body>
+</html>
